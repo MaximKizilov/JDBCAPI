4170f41045e35db293ca0e28b2bbb3adbf08ff10
kizilovms <kizilov2020@gmail.com>
2024-10-28 13:50:47 +1000

    Initial commit

--- src/main/resources/shema.sql
+++ src/main/resources/shema.sql
@@ -1,7 +1,15 @@
-CREATE TABLE CUSTOMERS (
+CREATE TABLE IF NOT EXISTS CUSTOMERS (
                            id SERIAL PRIMARY KEY,
                            name VARCHAR(50) NOT NULL,
                            surname VARCHAR(50) NOT NULL,
                            age INT CHECK(age >= 0),
                            phone_number VARCHAR(15)
+);
+CREATE TABLE IF NOT EXISTS ORDERS(
+                       id SERIAL PRIMARY KEY,
+                       date DATE DEFAULT CURRENT_DATE,
+                       customer_id int,
+                       product_name VARCHAR(30) NOT NULL,
+                       amount INT CHECK (amount >= 0),
+                       FOREIGN KEY (customer_id) REFERENCES customers (id)
 );
