0d8a7597c5c64a4fcb6529bbc77dc04c1ab35771
kizilovms <kizilov2020@gmail.com>
2024-10-28 12:24:33 +1000

    Initial commit

---
+++ src/main/java/com/example/springjdbc/controller/JDBCController.java
@@ -0,0 +1,7 @@
+package com.example.springjdbc.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JDBCController {
+}
---
+++ src/main/java/com/example/springjdbc/repository/JDBCRepository.java
@@ -0,0 +1,7 @@
+package com.example.springjdbc.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JDBCRepository {
+}
